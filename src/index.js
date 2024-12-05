8bfc7d7d31bf63dc6c89a27ce1965bff8ad247cf
Enriquedonaire <donaire.q2@gmail.com>
2024-12-05 22:12:35 +0100

    Fix 404

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/main.scss";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter basename="/Mytheresa-movie-App-Test">
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
