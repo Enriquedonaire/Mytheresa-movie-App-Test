84afd3e613b8fddc47c0997236815551fcb4733a
Enriquedonaire <donaire.q2@gmail.com>
2024-12-05 21:51:44 +0100

    Add hashRouter without basename

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles/main.scss";
 
-ReactDOM.render(
-    <HashRouter>
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <HashRouter >
         <App />
-    </HashRouter>,
-    document.getElementById('root')
+    </HashRouter>
 );
