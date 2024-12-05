e63956c12aeb3d08d4b074bd42424d78dcfc28ea
Enriquedonaire <donaire.q2@gmail.com>
2024-12-05 22:06:24 +0100

    Add news  _redirects and 404.html

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/main.scss";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <BrowserRouter basename="/Mytheresa-movie-App-Test">
+        <App />
+    </BrowserRouter>
 );
