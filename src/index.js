2432eab2be1b0efe5b1309877fd14fdd362eb8e5
Enriquedonaire <donaire.q2@gmail.com>
2024-12-05 22:00:55 +0100

    Remove unused _redirects and 404.html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/main.scss";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-    <HashRouter >
-        <App />
-    </HashRouter>
+ReactDOM.render(
+    <React.StrictMode>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
